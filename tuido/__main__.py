3582f399fab0046886a25ab0f788557ba7c52600
nautrw <nautrw@proton.me>
2024-11-11 11:54:29 -0500

    test again

--- tuido/__main__.py
+++ tuido/__main__.py
@@ -10,6 +10,8 @@ def main():
 
     database.create_table(connection)
     database.add_todo(connection, "test")
+    database.add_todo(connection, "test2", 1)
+    database.add_todo(connection, "test3", 0)
 
     todos_list = database.get_todos(connection)
     for todo in todos_list:
